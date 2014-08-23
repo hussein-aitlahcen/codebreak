a9e978985c9b3082531c7fb73cf57e58355e9c97
Noxivs <sum.noxivs@gmail.com>
2014-08-23 21:38:14 +0200

    début de de la structure de l'ia (pour implémenter le pvm si besoin)

---
+++ Src/Codebreak.Service.World/Game/Fight/AI/AIFighter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Codebreak.Service.World.Game.Entity;
+
+namespace Codebreak.Service.World.Game.Fight.AI
+{
+    public abstract class AIFighter : FighterBase
+    {
+        protected AIFighter(EntityTypEnum type, long id) : base(type, id)
+        {
+
+        }
+
+    }
+}
---
+++ Src/Codebreak.Service.World/Game/Fight/AI/Brains/DefaultAIBrain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codebreak.Service.World.Game.Fight.AI.Brains
+{
+    public sealed class DefaultAIBrain : AIBrain
+    {
+        public DefaultAIBrain(AIFighter fighter) : base(fighter)
+        {
+
+        }
+    }
+}
