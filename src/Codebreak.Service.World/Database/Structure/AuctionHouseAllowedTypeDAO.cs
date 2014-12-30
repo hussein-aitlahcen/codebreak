a376ca1549a505b0630ff347c10ced00939cf83f
Hussein Ait-Lahcen <hussein.aitlahcen@gmail.com>
2014-12-30 16:48:14 +0100

    Hotel de vente, possibilité d'echanger, ajouter un item à la vente, restriction sur : level item, type item, vente maximum par compte, afficher la liste des items d'un type et les paliers de vente organisés par prix croissant

---
+++ src/Codebreak.Service.World/Database/Structure/AuctionHouseAllowedTypeDAO.cs
@@ -0,0 +1,27 @@
+﻿using Codebreak.Framework.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codebreak.Service.World.Database.Structure
+{
+    [Table("auctionhouseallowedtype")]
+    public sealed class AuctionHouseAllowedTypeDAO : DataAccessObject<AuctionHouseAllowedTypeDAO>
+    {
+        [Key]
+        public int AuctionHouseId
+        {
+            get;
+            set;
+        }
+
+        [Key]
+        public int TemplateId
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ src/Codebreak.Service.World/Game/Exchange/AuctionHouseExchange.cs
@@ -0,0 +1,59 @@
+﻿using Codebreak.Service.World.Game.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codebreak.Service.World.Game.Exchange
+{
+    public abstract class AuctionHouseExchange : ExchangeBase
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public CharacterEntity Character
+        {
+            get;
+            private set;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public NonPlayerCharacterEntity Npc
+        {
+            get;
+            private set;
+        }
+
+         /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="character"></param>
+        /// <param name="npc"></param>
+        public AuctionHouseExchange(ExchangeTypeEnum type, CharacterEntity character, NonPlayerCharacterEntity npc)
+            : base(type)
+        {
+            Character = character;
+            Npc = npc;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        protected override string SerializeAs_ExchangeCreate()
+        {
+            return "1,10,100" + ";"
+                + String.Join(",", Npc.AuctionHouse.AllowedTypes) + ";"
+                + Npc.AuctionHouse.Taxe + ";"
+                + Npc.AuctionHouse.ItemMaxLevel + ";"
+                + Npc.AuctionHouse.PlayerMaxItem + ";"
+                + Npc.Id + ";"
+                + Npc.AuctionHouse.Timeout + "|"
+                + Npc.Id;
+        }
+    }
+}
