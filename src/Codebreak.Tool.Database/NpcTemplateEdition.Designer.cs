0030b6c160d6ffb07330854b26a40b824bc9ee5a
Hussein Ait-Lahcen <hussein.aitlahcen@gmail.com>
2015-01-15 16:11:02 +0100

    Ajout de la fênetre d'édition des npcs

---
+++ src/Codebreak.Tool.Database/NpcTemplateEdition.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace Codebreak.Tool.Database
+{
+    partial class NpcTemplateEdition
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSaveSellList = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.btnAddToSell = new System.Windows.Forms.Button();
+            this.listViewSellList = new System.Windows.Forms.ListView();
+            this.columnId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnInsertInCurrent = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabelCurrentItemName = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.btnSaveRewards = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnSaveSellList);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.btnAddToSell);
+            this.groupBox1.Controls.Add(this.listViewSellList);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(395, 491);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Vente";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(99, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ItemId :";
+            // 
+            // btnSaveSellList
+            // 
+            this.btnSaveSellList.Location = new System.Drawing.Point(311, 18);
+            this.btnSaveSellList.Name = "btnSaveSellList";
+            this.btnSaveSellList.Size = new System.Drawing.Size(78, 26);
+            this.btnSaveSellList.TabIndex = 3;
+            this.btnSaveSellList.Text = "Sauvegarder";
+            this.btnSaveSellList.UseVisualStyleBackColor = true;
+            this.btnSaveSellList.Click += new System.EventHandler(this.btnSaveSellList_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(147, 21);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(63, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // btnAddToSell
+            // 
+            this.btnAddToSell.Location = new System.Drawing.Point(6, 18);
+            this.btnAddToSell.Name = "btnAddToSell";
+            this.btnAddToSell.Size = new System.Drawing.Size(87, 26);
+            this.btnAddToSell.TabIndex = 1;
+            this.btnAddToSell.Text = "Ajouter";
+            this.btnAddToSell.UseVisualStyleBackColor = true;
+            this.btnAddToSell.Click += new System.EventHandler(this.btnAddToSell_Click);
+            // 
+            // listViewSellList
+            // 
+            this.listViewSellList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnId,
+            this.columnName});
+            this.listViewSellList.FullRowSelect = true;
+            this.listViewSellList.GridLines = true;
+            this.listViewSellList.Location = new System.Drawing.Point(6, 50);
+            this.listViewSellList.Name = "listViewSellList";
+            this.listViewSellList.Size = new System.Drawing.Size(383, 435);
+            this.listViewSellList.TabIndex = 0;
+            this.listViewSellList.UseCompatibleStateImageBehavior = false;
+            this.listViewSellList.View = System.Windows.Forms.View.Details;
+            this.listViewSellList.KeyUp += new System.Windows.Forms.KeyEventHandler(this.listViewSellList_KeyUp);
+            // 
+            // columnId
+            // 
+            this.columnId.Text = "ItemId";
+            this.columnId.Width = 84;
+            // 
+            // columnName
+            // 
+            this.columnName.Text = "Name";
+            this.columnName.Width = 281;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnSaveRewards);
+            this.groupBox2.Controls.Add(this.numericUpDown3);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.numericUpDown2);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Controls.Add(this.btnInsertInCurrent);
+            this.groupBox2.Controls.Add(this.treeView1);
+            this.groupBox2.Location = new System.Drawing.Point(413, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(612, 485);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Echange";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Type :";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "item",
+            "kamas",
+            "entry"});
+            this.comboBox1.Location = new System.Drawing.Point(129, 20);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(87, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // btnInsertInCurrent
+            // 
+            this.btnInsertInCurrent.Location = new System.Drawing.Point(6, 18);
+            this.btnInsertInCurrent.Name = "btnInsertInCurrent";
+            this.btnInsertInCurrent.Size = new System.Drawing.Size(75, 26);
+            this.btnInsertInCurrent.TabIndex = 1;
+            this.btnInsertInCurrent.Text = "Inserer";
+            this.btnInsertInCurrent.UseVisualStyleBackColor = true;
+            this.btnInsertInCurrent.Click += new System.EventHandler(this.btnInsertInCurrent_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(6, 50);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(600, 429);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.treeView1_KeyUp);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabelCurrentItemName});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 506);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1037, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabelCurrentItemName
+            // 
+            this.toolStripStatusLabelCurrentItemName.ForeColor = System.Drawing.Color.Red;
+            this.toolStripStatusLabelCurrentItemName.Name = "toolStripStatusLabelCurrentItemName";
+            this.toolStripStatusLabelCurrentItemName.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabelCurrentItemName.Text = "Nom de l\'item actuel";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(222, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Valeur :";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(271, 21);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            1316134912,
+            2328,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(88, 20);
+            this.numericUpDown2.TabIndex = 5;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(365, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Quantité :";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(424, 21);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            1316134912,
+            2328,
+            0,
+            0});
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(88, 20);
+            this.numericUpDown3.TabIndex = 7;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // btnSaveRewards
+            // 
+            this.btnSaveRewards.Location = new System.Drawing.Point(518, 18);
+            this.btnSaveRewards.Name = "btnSaveRewards";
+            this.btnSaveRewards.Size = new System.Drawing.Size(88, 26);
+            this.btnSaveRewards.TabIndex = 8;
+            this.btnSaveRewards.Text = "Sauvegarder";
+            this.btnSaveRewards.UseVisualStyleBackColor = true;
+            this.btnSaveRewards.Click += new System.EventHandler(this.btnSaveRewards_Click);
+            // 
+            // NpcTemplateEdition
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1037, 528);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "NpcTemplateEdition";
+            this.Text = "Oklm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.NpcTemplateEdition_FormClosing);
+            this.Load += new System.EventHandler(this.NpcTemplateEdition_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView listViewSellList;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button btnAddToSell;
+        private System.Windows.Forms.ColumnHeader columnId;
+        private System.Windows.Forms.ColumnHeader columnName;
+        private System.Windows.Forms.Button btnSaveSellList;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelCurrentItemName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnInsertInCurrent;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSaveRewards;
+    }
+}
---
+++ src/Codebreak.Tool.Database/NpcTemplateEdition.cs
@@ -0,0 +1,340 @@
+﻿using Codebreak.Service.World.Database.Repository;
+using Codebreak.Service.World.Database.Structure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Codebreak.Tool.Database
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class NpcTemplateEdition : Form
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private NpcTemplateDAO m_npcTemplate;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="npcTemplate"></param>
+        public NpcTemplateEdition(NpcTemplateDAO npcTemplate)
+        {
+            m_npcTemplate = npcTemplate;
+            
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NpcTemplateEdition_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+            foreach (var item in m_npcTemplate.ShopList)
+                AddToSell(item);
+            ReloadRewards();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void ReloadRewards()
+        {
+            treeView1.Nodes.Clear();
+            foreach (var reward in m_npcTemplate.Rewards)
+                AddReward(reward);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="reward"></param>
+        private void AddReward(RewardEntry reward)
+        {
+            var mainNode = new TreeNode(reward.GetHashCode().ToString())
+            {
+                Tag = reward
+            };
+
+            var requiredNode = new TreeNode("required")
+            {
+                Tag = reward.RequiredItems
+            };
+
+            if(reward.RequiredKamas > 0)
+                requiredNode.Nodes.Add(reward.RequiredKamas + " kamas");
+            foreach (var item in reward.RequiredItems)
+                requiredNode.Nodes.Add(new TreeNode(item.Quantity + "x " + item.Template.Name)
+                {
+                    Tag = item
+                });
+
+            var rewardedNode = new TreeNode("rewarded")
+            {
+                Tag = reward.RewardedItems
+            };
+
+            if (reward.RewardedKamas > 0)
+            rewardedNode.Nodes.Add(reward.RewardedKamas + " kamas");
+            foreach(var item in reward.RewardedItems)            
+                rewardedNode.Nodes.Add(new TreeNode(item.Quantity + "x " + item.Template.Name)
+                    {
+                        Tag = item
+                    });
+
+            mainNode.Nodes.Add(requiredNode);
+            mainNode.Nodes.Add(rewardedNode);
+            treeView1.Nodes.Add(mainNode);
+            treeView1.ExpandAll();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void AddToSell(ItemTemplateDAO item)
+        {
+            listViewSellList.Items.Add(new ListViewItem(new string[] {
+                item.Id.ToString(),
+                item.Name
+            })
+            {
+                Tag = item
+            });
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAddToSell_Click(object sender, EventArgs e)
+        {
+            var itemTemplate = ItemTemplateRepository.Instance.GetById((int)numericUpDown1.Value);
+            if (itemTemplate == null)
+            {
+                MessageBox.Show("Item inéxistant.");
+                return;
+            }
+            else if(m_npcTemplate.ShopList.Any(item => item.Id == itemTemplate.Id))
+            {
+                MessageBox.Show("Item déjà en vente.");
+                return;
+            }
+
+            AddToSell(itemTemplate);
+            m_npcTemplate.ShopList.Add(itemTemplate);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSaveSellList_Click(object sender, EventArgs e)
+        {
+            m_npcTemplate.Sell = string.Join(",", m_npcTemplate.ShopList.Select(item => item.Id));
+            if(!m_npcTemplate.Save())
+            {
+                MessageBox.Show("Impossible de sauvegarder.");
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+             var itemTemplate = ItemTemplateRepository.Instance.GetById((int)numericUpDown1.Value);
+            if (itemTemplate == null)
+            {
+                toolStripStatusLabelCurrentItemName.Text = "Item inéxistant";
+                toolStripStatusLabelCurrentItemName.ForeColor = Color.Red;
+            }
+            else
+            {
+                toolStripStatusLabelCurrentItemName.Text = itemTemplate.Name;
+                toolStripStatusLabelCurrentItemName.ForeColor = Color.DarkGreen;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void listViewSellList_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Delete)
+            {
+                foreach(ListViewItem item in listViewSellList.SelectedItems)
+                {
+                    var template = item.Tag as ItemTemplateDAO;
+                    m_npcTemplate.ShopList.Remove(template);
+                    listViewSellList.Items.Remove(item);
+                }
+            }
+        }
+
+     
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedText == "item")
+            {
+                var itemTemplate = ItemTemplateRepository.Instance.GetById((int)numericUpDown2.Value);
+                if (itemTemplate == null)
+                {
+                    toolStripStatusLabelCurrentItemName.Text = "Item inéxistant";
+                    toolStripStatusLabelCurrentItemName.ForeColor = Color.Red;
+                }
+                else
+                {
+                    toolStripStatusLabelCurrentItemName.Text = itemTemplate.Name;
+                    toolStripStatusLabelCurrentItemName.ForeColor = Color.DarkGreen;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnInsertInCurrent_Click(object sender, EventArgs e)
+        {            
+            var node = treeView1.SelectedNode;
+            switch(comboBox1.SelectedItem.ToString())
+            {
+                case "kamas":
+                    if (node == null || (node.Text != "rewarded" && node.Text != "required"))
+                    {
+                        MessageBox.Show("Selectionnez un noeud 'required' ou 'rewarded' pour ajouter des kamas.");
+                        return;
+                    }
+
+                    var reward = node.Parent.Tag as RewardEntry;
+                    if(node.Text == "rewarded")
+                    {
+                        reward.RewardedKamas = (long)numericUpDown3.Value;
+                    }
+                    else if (node.Text == "required")
+                    {                        
+                        reward.RequiredKamas = (long)numericUpDown3.Value;
+                    }                
+                    break;
+
+
+                case "item":
+                    if (node == null || node.Tag == null || (node.Tag as List<RewardEntry.ItemEntry>) == null)
+                    {
+                        MessageBox.Show("Selectionnez un noeud 'required' ou 'rewarded' pour ajouter un item.");
+                        return;
+                    }
+
+                    var itemTemplate = ItemTemplateRepository.Instance.GetById((int)numericUpDown2.Value);
+                    if (itemTemplate == null)
+                    {
+                        MessageBox.Show("Item inéxistant.");
+                        return;
+                    }
+
+                    var list = node.Tag as List<RewardEntry.ItemEntry>;
+                    reward = node.Parent.Tag as RewardEntry;
+                    if(list.Any(item => item.TemplateId == itemTemplate.Id))
+                    {
+                        MessageBox.Show("Item déjà présent.");
+                        return;
+                    }
+
+                    list.Add(new RewardEntry.ItemEntry(itemTemplate.Id, (long)numericUpDown3.Value));
+                    break;
+                    
+                case "entry":
+                    m_npcTemplate.Rewards.Add(new RewardEntry());
+                    break;
+            }
+
+            ReloadRewards();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSaveRewards_Click(object sender, EventArgs e)
+        {
+            m_npcTemplate.Exchange = string.Join("|", m_npcTemplate.Rewards.Select(reward => reward.Serialize()));
+            if (!m_npcTemplate.Save())
+            {
+                MessageBox.Show("Impossible de sauvegarder.");
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NpcTemplateEdition_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            btnSaveSellList_Click(null, null);
+            btnSaveRewards_Click(null, null);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void treeView1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                var node = treeView1.SelectedNode;
+                var rewardEntry = node.Tag as RewardEntry;
+                if (rewardEntry != null)
+                {
+                    m_npcTemplate.Rewards.Remove(rewardEntry);
+                }
+                else
+                {
+                    rewardEntry = node.Parent.Tag as RewardEntry;
+                    if (node.Text == "required")
+                    {
+                        (node.Tag as List<RewardEntry.ItemEntry>).Clear();
+                        rewardEntry.RequiredKamas = 0;
+                    }
+                    else if (node.Text == "rewarded")
+                    {
+                        (node.Tag as List<RewardEntry.ItemEntry>).Clear();
+                        rewardEntry.RewardedKamas = 0;
+                    }
+                    else if((node.Tag as RewardEntry.ItemEntry) != null)
+                    {
+                        (node.Parent.Tag as List<RewardEntry.ItemEntry>).Remove(node.Tag as RewardEntry.ItemEntry);
+                    }
+                }              
+
+                ReloadRewards();
+            }
+        }
+    }
+}
