6175ec1870e24a42135da3aea234277ac06d301f
Hussein Ait-Lahcen <hussein.aitlahcen@gmail.com>
2015-01-15 16:02:28 +0100

    Corrections convertisseurs, creation automatique des dossiers inexistants

--- src/Codebreak.Tool.Convert/Program.cs
+++ src/Codebreak.Tool.Convert/Program.cs
@@ -11,13 +11,20 @@ namespace Codebreak.Tool.Convert
     class Program
     {
         static string INPUT_FOLDER = "./input/";
-        static string OUTPUT_FILE = "./output/result.sql";
+        static string OUTPUT_FOLDER = "./output/";
+        static string OUTPUT_FILE = OUTPUT_FOLDER + "result.sql";
         static string REAL_QUERY = "insert into maptemplate values ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}', '{9}', '{10}');";
         static string REGEX_QUERY = @"VALUES \('(?<Id>[^']*)', '(?<CreateTime>[^']*)', '(?<Width>[^']*)', '(?<Height>[^']*)', '(?<Data>[^']*)', '(?<DataKey>[^']*)', '(?<Places>[^']*)', '(?<Monsters>[^']*)', '(?<Capabilities>[^']*)', '(?<Position>[^']*)',";
         
         static void Main(string[] args)
         {
             Console.WriteLine("Loading...");
+
+            if (!Directory.Exists(INPUT_FOLDER))
+                Directory.CreateDirectory(INPUT_FOLDER);
+
+            if(!Directory.Exists(OUTPUT_FOLDER))
+                Directory.CreateDirectory(OUTPUT_FOLDER);
 
             Regex expression = new Regex(REGEX_QUERY, RegexOptions.None);
             var inputFiles = Directory.GetFiles(INPUT_FOLDER, "*.sql");
