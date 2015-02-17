00a200657ff9c90d1b9513fd7d930c1926f527b7
Hussein Ait-Lahcen <hussein.aitlahcen@gmail.com>
2015-02-17 09:04:24 +0100

    Correction de la condition 'inutilisable'

--- src/Codebreak.Service.World/Game/Condition/ConditionParser.cs
+++ src/Codebreak.Service.World/Game/Condition/ConditionParser.cs
@@ -50,60 +50,65 @@ namespace Codebreak.Service.World.Game.Condition
                 }
                 else
                 {
-                    Regex hasTemplateRegex = new Regex(@"PO==(?<HasTemplate>[0-9]*)", RegexOptions.None);
-                    Regex notHasTemplateRegex = new Regex(@"PO!=(?<NotHasTemplate>[0-9]*)", RegexOptions.None);
-
-                    var subCond = hasTemplateRegex.Replace(conditions, @"character.Inventory.HasTemplate(${HasTemplate})");
-                    subCond = notHasTemplateRegex.Replace(subCond, @"character.Inventory.NotHasTemplate(${NotHasTemplate})");
-                    
-                    // Nouvelle
-                    StringBuilder realConditions = new StringBuilder(subCond);
-
-                    // Stats tot
-                    realConditions.Replace("CI", "character.Statistics.GetTotal(EffectEnum.AddIntelligence)");
-                    realConditions.Replace("CV", "character.Statistics.GetTotal(EffectEnum.AddVitality)");
-                    realConditions.Replace("CA", "character.Statistics.GetTotal(EffectEnum.AddAgility)");
-                    realConditions.Replace("CW", "character.Statistics.GetTotal(EffectEnum.AddWisdom)");
-                    realConditions.Replace("CC", "character.Statistics.GetTotal(EffectEnum.AddChance)");
-                    realConditions.Replace("CS", "character.Statistics.GetTotal(EffectEnum.AddStrength)");
-
-                    // Stats base
-                    realConditions.Replace("Ci", "character.DatabaseRecord.Intelligence");
-                    realConditions.Replace("Cs", "character.DatabaseRecord.Strength");
-                    realConditions.Replace("Cv", "character.DatabaseRecord.Vitality");
-                    realConditions.Replace("Ca", "character.DatabaseRecord.Agility");
-                    realConditions.Replace("Cw", "character.DatabaseRecord.Wisdom");
-                    realConditions.Replace("Cc", "character.DatabaseRecord.Chance");
-
-                    // Perso
-                    realConditions.Replace("Ps", "character.AlignmentId");
-                    realConditions.Replace("Pa", "character.AlignmentLevel");
-                    realConditions.Replace("PP", "character.AlignmentPromotion");
-                    realConditions.Replace("PL", "character.Level");
-                    realConditions.Replace("PK", "character.Inventory.Kamas");
-                    realConditions.Replace("PG", "character.BreedId");
-                    realConditions.Replace("PS", "character.Sex");
-                    realConditions.Replace("PZ", "1"); // Abonné
-                    realConditions.Replace("PN", "character.Name");
-                    realConditions.Replace("Pg", "0"); // Don
-                    realConditions.Replace("PR", "0"); // Married 
-                    if (character.Account != null)
-                        realConditions.Replace("PX", "character.Account.Power"); // Admin level
-                    realConditions.Replace("PW", "10000"); // MaxWeight
-                    if (character.Map != null)
-                        realConditions.Replace("PB", "character.Map.SubAreaId");
-
-                    realConditions.Replace("SI", "character.MapId");
-                    realConditions.Replace("MiS", "character.Id");
-                    realConditions.Replace("BI", "0"); // ?
-
-                    var registry = new TypeRegistry();
-                    registry.RegisterType("EffectEnum", typeof(EffectEnum));
-
-                    method = new CompiledExpression<bool>(realConditions.ToString())
+                    if (conditions.Contains("BI")) // inutilisable
                     {
-                        TypeRegistry = registry
-                    }.ScopeCompile<ConditionScope>();                 
+                        method = new Func<ConditionScope, bool>((scope) => false);
+                    }
+                    else
+                    {
+                        Regex hasTemplateRegex = new Regex(@"PO==(?<HasTemplate>[0-9]*)", RegexOptions.None);
+                        Regex notHasTemplateRegex = new Regex(@"PO!=(?<NotHasTemplate>[0-9]*)", RegexOptions.None);
+
+                        var subCond = hasTemplateRegex.Replace(conditions, @"character.Inventory.HasTemplate(${HasTemplate})");
+                        subCond = notHasTemplateRegex.Replace(subCond, @"character.Inventory.NotHasTemplate(${NotHasTemplate})");
+
+                        // Nouvelle
+                        StringBuilder realConditions = new StringBuilder(subCond);
+
+                        // Stats tot
+                        realConditions.Replace("CI", "character.Statistics.GetTotal(EffectEnum.AddIntelligence)");
+                        realConditions.Replace("CV", "character.Statistics.GetTotal(EffectEnum.AddVitality)");
+                        realConditions.Replace("CA", "character.Statistics.GetTotal(EffectEnum.AddAgility)");
+                        realConditions.Replace("CW", "character.Statistics.GetTotal(EffectEnum.AddWisdom)");
+                        realConditions.Replace("CC", "character.Statistics.GetTotal(EffectEnum.AddChance)");
+                        realConditions.Replace("CS", "character.Statistics.GetTotal(EffectEnum.AddStrength)");
+
+                        // Stats base
+                        realConditions.Replace("Ci", "character.DatabaseRecord.Intelligence");
+                        realConditions.Replace("Cs", "character.DatabaseRecord.Strength");
+                        realConditions.Replace("Cv", "character.DatabaseRecord.Vitality");
+                        realConditions.Replace("Ca", "character.DatabaseRecord.Agility");
+                        realConditions.Replace("Cw", "character.DatabaseRecord.Wisdom");
+                        realConditions.Replace("Cc", "character.DatabaseRecord.Chance");
+
+                        // Perso
+                        realConditions.Replace("Ps", "character.AlignmentId");
+                        realConditions.Replace("Pa", "character.AlignmentLevel");
+                        realConditions.Replace("PP", "character.AlignmentPromotion");
+                        realConditions.Replace("PL", "character.Level");
+                        realConditions.Replace("PK", "character.Inventory.Kamas");
+                        realConditions.Replace("PG", "character.BreedId");
+                        realConditions.Replace("PS", "character.Sex");
+                        realConditions.Replace("PZ", "1"); // Abonné
+                        realConditions.Replace("PN", "character.Name");
+                        realConditions.Replace("Pg", "0"); // Don
+                        realConditions.Replace("PR", "0"); // Married 
+                        if (character.Account != null)
+                            realConditions.Replace("PX", "character.Account.Power"); // Admin level
+                        realConditions.Replace("PW", "10000"); // MaxWeight
+                        if (character.Map != null)
+                            realConditions.Replace("PB", "character.Map.SubAreaId");
+                        realConditions.Replace("SI", "character.MapId");
+                        realConditions.Replace("MiS", "character.Id");
+
+                        var registry = new TypeRegistry();
+                        registry.RegisterType("EffectEnum", typeof(EffectEnum));
+
+                        method = new CompiledExpression<bool>(realConditions.ToString())
+                        {
+                            TypeRegistry = registry
+                        }.ScopeCompile<ConditionScope>();
+                    }
                     m_compiledExpressions.Add(conditions, method);
                 }
             }
