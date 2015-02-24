59a49150a9d1555c82cd042cb611b2353c056b16
Hussein Ait-Lahcen <hussein.aitlahcen@gmail.com>
2015-02-24 12:18:39 +0100

    Amelioration mineure IA

--- src/Codebreak.Service.World/Game/Fight/AI/Action/Type/MoveAction.cs
+++ src/Codebreak.Service.World/Game/Fight/AI/Action/Type/MoveAction.cs
@@ -22,6 +22,18 @@ namespace Codebreak.Service.World.Game.Fight.AI.Action.Type
             set;
         }
 
+        private string StringPath
+        {
+            get;
+            set;
+        }
+
+        private MovementPath Path
+        {
+            get;
+            set;
+        }
+
         private int CellId
         {
             get; 
@@ -51,24 +63,30 @@ namespace Codebreak.Service.World.Game.Fight.AI.Action.Type
             switch (MoveState)
             {
                 case MoveStateEnum.STATE_CALCULATE_CELL:
-                    CellId = Fighter.Team.OpponentTeam.AliveFighters.OrderBy(fighter => fighter.Life).First().Cell.Id;
+                    var WeakestEnnemies = Fighter.Team.OpponentTeam.AliveFighters.OrderBy(fighter => Pathfinding.GoalDistance(Map, Fighter.Cell.Id, fighter.Cell.Id));
+                    foreach(var ennemy in WeakestEnnemies)
+                    {
+                        StringPath = Fighter.Fight.Map.Pathmaker.FindPathAsString(Fighter.Cell.Id, ennemy.Cell.Id, false, Fighter.MP, Fighter.Fight.Obstacles);
+                        if (StringPath == string.Empty)
+                            continue;
+
+                        Path = Fighter.Fight.Map.DecodeMovement(Fighter, ennemy.Cell.Id, StringPath);
+                        if (Path != null)
+                            break;
+                    }
+
                     MoveState = MoveStateEnum.STATE_MOVE;
 
                     return AIActionResult.RUNNING;
 
                 case MoveStateEnum.STATE_MOVE:
 
-                    var stringPath = Fighter.Fight.Map.Pathmaker.FindPathAsString(Fighter.Cell.Id, CellId, false, Fighter.MP, Fighter.Fight.Obstacles);
-                    if (stringPath == string.Empty)
+                    if (StringPath == null || StringPath == string.Empty || Path == null)
                         return AIActionResult.FAILURE;
 
-                    var path = Fighter.Fight.Map.DecodeMovement(Fighter, Fighter.Cell.Id, stringPath);
-                    if (path == null)
-                        return AIActionResult.FAILURE;
-
-                    CellId = path.EndCell;
-                    Timeout = (int)path.MovementTime;
-                    Fighter.Fight.Move(Fighter, Fighter.Cell.Id, stringPath);
+                    CellId = Path.EndCell;
+                    Timeout = (int)Path.MovementTime;
+                    Fighter.Fight.Move(Fighter, Fighter.Cell.Id, StringPath);
 
                     MoveState = MoveStateEnum.STATE_MOVING;
 
